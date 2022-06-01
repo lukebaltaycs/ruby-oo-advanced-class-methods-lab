ce4e822bceab7f74079fd9b1f6511bf0380042b5
Luke Baltay <luke.baltay@yale.edu>
2022-06-01 11:46:09 -0400

    commit tree 58c3f564882b56927ef1e314e9c27f722879cdaf

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,57 @@ class Song
     @@all
   end
 
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+
+  def initialize(name="", artist_name="")
+    @name=name
+    @artist_name=artist_name
+  end
+
+  def self.new_by_name(name)
+    Song.new(name)
+  end
+
+  def self.create_by_name(name)
+    song = Song.new(name)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name_given)
+    @@all.select{|song| song.name == name_given}[0]
+  end
+
+  def self.find_or_create_by_name(name_given)
+    Song.create_by_name(name_given)
+    Song.find_by_name(name_given)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{ |song| song.name}
+  end
+
+  def self.create_from_filename(filename)
+    filename_split = filename.split(/[-.]/)
+    new=Song.create_by_name(filename_split[1].strip)
+    new.artist_name=filename_split[0].strip
+  end
+
+  def self.new_from_filename(filename)
+    filename_split = filename.split(/[-.]/)
+    Song.new(filename_split[1].strip, filename_split[0].strip)
+  end
+
+  def self.destroy_all
+    @@all=[]
+  end
+
+
+
   def save
     self.class.all << self
   end
